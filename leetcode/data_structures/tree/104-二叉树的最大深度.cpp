1e3166436335163fc08a45cf9e7af42448b06d2c
zhm_linux <zhm1019@qq.com>
2024-01-25 18:55:54 +0800

    Add solution for finding maximum depth of a binary tree

---
+++ leetcode/data_structures/tree/104-二叉树的最大深度.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  explicit TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  int maxDepth(TreeNode *root) {
+    if (root == nullptr) return 0;
+
+    return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+  }
+};
