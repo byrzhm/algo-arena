cbdf06d8e915cb58774b2ae73402b869ab6f41f6
byrzhm <92535690+byrzhm@users.noreply.github.com>
2024-02-11 12:30:59 +0800

    {}

--- algs4-princeton/wordnet/OutCast.java
+++ algs4-princeton/wordnet/OutCast.java
@@ -1,5 +1,5 @@
 public class Outcast {
-   public Outcast(WordNet wordnet)         // constructor takes a WordNet object
-   public String outcast(String[] nouns)   // given an array of WordNet nouns, return an outcast
-   public static void main(String[] args)  // see test client below
+   public Outcast(WordNet wordnet){}         // constructor takes a WordNet object
+   public String outcast(String[] nouns){}   // given an array of WordNet nouns, return an outcast
+   public static void main(String[] args){}  // see test client below
 }
