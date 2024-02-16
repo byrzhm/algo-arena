4b2dfbf313e21dd776881c474f5e8423f001d340
zhm_linux <zhm1019@qq.com>
2024-02-16 13:51:29 +0800

    Add binary search tree test and benchmark files

--- examples/data_structures/binary_search_tree/benchmark.cpp
+++ examples/data_structures/binary_search_tree/benchmark.cpp
@@ -1,6 +1,91 @@
+#include <algorithm>
 #include <benchmark/benchmark.h>
 #include <map>
+#include <random>
+#include <vector>
+
+#include "bst.hpp"
+#include "rbtree.hpp"
 
 // TODO(zhm): benchmark rbtree
+
+std::vector<int> getSortedVec(int size)
+{
+  std::vector<int> vec(size);
+  for (int i = 0; i < size; ++i) {
+    vec[i] = size;
+  }
+  return vec;
+}
+
+std::vector<int> getRandomVec(int size)
+{
+  std::vector<int> vec = getSortedVec(size);
+  std::shuffle(vec.begin(), vec.end(), std::mt19937(std::random_device()()));
+  return vec;
+}
+
+static void BM_BSTPutSorted(benchmark::State &state)
+{
+  auto vec = getSortedVec(state.range());
+  for (auto _ : state) {
+    BST<int, int> tree;
+    for (auto num : vec) {
+      tree.put(num, num);
+    }
+  }
+}
+
+static void BM_BSTPutRandom(benchmark::State &state)
+{
+  auto vec = getRandomVec(state.range());
+  for (auto _ : state) {
+    BST<int, int> tree;
+    for (auto num : vec) {
+      tree.put(num, num);
+    }
+  }
+}
+
+static void BM_BSTGetSorted(benchmark::State &state)
+{
+  int size = state.range();
+  std::vector<int> vec = getSortedVec(size);
+  BST<int, int> tree;
+  for (int num : vec) {
+    tree.put(num, num);
+  }
+
+  std::mt19937 gen(std::random_device{}());
+  std::uniform_int_distribution<> dis(0, size - 1);
+
+  for (auto _ : state) {
+    int random_key = vec[dis(gen)];
+    benchmark::DoNotOptimize(tree.get(random_key));
+  }
+}
+
+static void BM_BSTGetRandom(benchmark::State &state)
+{
+  int size = state.range();
+  std::vector<int> vec = getRandomVec(size);
+  BST<int, int> tree;
+  for (int num : vec) {
+    tree.put(num, num);
+  }
+
+  std::mt19937 gen(std::random_device{}());
+  std::uniform_int_distribution<> dis(0, size - 1);
+
+  for (auto _ : state) {
+    int random_key = vec[dis(gen)];
+    benchmark::DoNotOptimize(tree.get(random_key));
+  }
+}
+
+BENCHMARK(BM_BSTPutSorted)->Range(8, 8 << 18);
+BENCHMARK(BM_BSTPutRandom)->Range(8, 8 << 18);
+BENCHMARK(BM_BSTGetSorted)->Range(8, 8 << 18);
+BENCHMARK(BM_BSTGetRandom)->Range(8, 8 << 18);
 
 BENCHMARK_MAIN();
---
+++ examples/data_structures/binary_search_tree/test.cpp
@@ -0,0 +1,71 @@
+#include "bst.hpp"
+#include <gtest/gtest.h>
+
+#include <random>
+
+TEST(BSTTest, basic)
+{
+  BST<int, int> tree;
+
+  // put
+  tree.put(1, 10);
+  tree.put(2, 20);
+
+  // contains
+  EXPECT_EQ(tree.contains(1), true);
+  EXPECT_EQ(tree.contains(2), true);
+  EXPECT_EQ(tree.contains(3), false);
+
+  // get
+  EXPECT_EQ(*tree.get(1), 10);
+  EXPECT_EQ(*tree.get(2), 20);
+  EXPECT_EQ(tree.get(3), nullptr);
+
+  EXPECT_EQ(tree.size(), 2);
+
+  // deleteMin/deleteMax
+  tree.deleteMin();
+  EXPECT_EQ(tree.get(1), nullptr);
+  EXPECT_EQ(*tree.get(2), 20);
+  EXPECT_EQ(tree.size(), 1);
+
+  tree.deleteMax();
+  EXPECT_EQ(tree.get(1), nullptr);
+  EXPECT_EQ(tree.get(2), nullptr);
+  EXPECT_EQ(tree.size(), 0);
+
+  EXPECT_EQ(tree.isEmpty(), true);
+}
+
+TEST(BSTTest, randomPut)
+{
+  BST<int, int> tree;
+  std::vector<int> input;
+  input.reserve(10);
+for (int i = 0; i < 10; ++i) {
+    input.emplace_back(i);
+  }
+
+  std::shuffle(input.begin(), input.end(), std::mt19937(std::random_device()()));
+
+  for (auto num : input) {
+    tree.put(num, num);
+  }
+
+  EXPECT_EQ(tree.size(), 10);
+  EXPECT_EQ(tree.min(), 0);
+  EXPECT_EQ(tree.max(), 9);
+
+  int num = 0;
+  for (auto it = tree.iterator(); it.hasNext();) {
+    auto val = *it.next();
+    EXPECT_EQ(val, num++);
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
