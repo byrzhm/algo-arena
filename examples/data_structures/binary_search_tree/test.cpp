f36f0cb9b1818bb4529ede90fdb0f41c4c76f6f1
zhm_linux <zhm1019@qq.com>
2024-02-16 18:36:34 +0800

    Refactor binary search tree and add red-black tree implementation

--- examples/data_structures/binary_search_tree/test.cpp
+++ examples/data_structures/binary_search_tree/test.cpp
@@ -1,40 +1,55 @@
-#include "bst.hpp"
 #include <gtest/gtest.h>
-
 #include <random>
+
+#include "bst.hpp"
+#include "rbtree.hpp"
 
 TEST(BSTTest, basic)
 {
   BST<int, int> tree;
 
   // put
-  tree.put(1, 10);
-  tree.put(2, 20);
+  for (int i = 0; i < 8; ++i) {
+    tree.put(i, i);
+  }
 
   // contains
-  EXPECT_EQ(tree.contains(1), true);
-  EXPECT_EQ(tree.contains(2), true);
-  EXPECT_EQ(tree.contains(3), false);
+  for (int i = 0; i < 8; ++i) {
+    EXPECT_TRUE(tree.contains(i)) << "at index " << i << '\n';
+  }
+  EXPECT_FALSE(tree.contains(88));
+  EXPECT_FALSE(tree.contains(-88));
 
   // get
-  EXPECT_EQ(*tree.get(1), 10);
-  EXPECT_EQ(*tree.get(2), 20);
-  EXPECT_EQ(tree.get(3), nullptr);
+  for (int i = 0; i < 8; ++i) {
+    EXPECT_EQ(*tree.get(i), i);
+  }
+  EXPECT_EQ(tree.get(88), nullptr);
+  EXPECT_EQ(tree.get(-88), nullptr);
 
-  EXPECT_EQ(tree.size(), 2);
+  EXPECT_EQ(tree.size(), 8);
 
   // deleteMin/deleteMax
   tree.deleteMin();
-  EXPECT_EQ(tree.get(1), nullptr);
-  EXPECT_EQ(*tree.get(2), 20);
-  EXPECT_EQ(tree.size(), 1);
+  EXPECT_EQ(tree.get(0), nullptr);
+  for (int i = 1; i < 8; ++i) {
+    EXPECT_EQ(*tree.get(i), i);
+  }
+  EXPECT_EQ(tree.size(), 7);
 
   tree.deleteMax();
-  EXPECT_EQ(tree.get(1), nullptr);
-  EXPECT_EQ(tree.get(2), nullptr);
-  EXPECT_EQ(tree.size(), 0);
+  EXPECT_EQ(tree.get(0), nullptr);
+  EXPECT_EQ(tree.get(7), nullptr);
+  for (int i = 1; i <= 6; ++i) {
+    EXPECT_EQ(*tree.get(i), i);
+  }
 
-  EXPECT_EQ(tree.isEmpty(), true);
+  EXPECT_EQ(tree.size(), 6);
+
+  for (int i = 1; i <= 6; ++i) {
+    tree.remove(i);
+  }
+  EXPECT_TRUE(tree.isEmpty());
 }
 
 TEST(BSTTest, randomPut)
@@ -42,7 +57,7 @@ TEST(BSTTest, randomPut)
   BST<int, int> tree;
   std::vector<int> input;
   input.reserve(10);
-for (int i = 0; i < 10; ++i) {
+  for (int i = 0; i < 10; ++i) {
     input.emplace_back(i);
   }
 
@@ -63,7 +78,8 @@ for (int i = 0; i < 10; ++i) {
   }
 }
 
-TEST(BSTTest, shapeTest) {
+TEST(BSTTest, shapeTest)
+{
   BST<int, int> tree;
 
   for (int i = 0; i < 10; ++i) {
@@ -72,6 +88,56 @@ TEST(BSTTest, shapeTest) {
 
   EXPECT_TRUE(tree.rightLinear());
   EXPECT_EQ(tree.height(), 9);
+}
+
+TEST(RBTreeTest, basic)
+{
+  RBTree<int, int> tree;
+
+  // put
+  for (int i = 0; i < 8; ++i) {
+    tree.put(i, i);
+  }
+
+  // contains
+  for (int i = 0; i < 8; ++i) {
+    EXPECT_TRUE(tree.contains(i)) << "at index " << i << '\n';
+  }
+  EXPECT_FALSE(tree.contains(88));
+  EXPECT_FALSE(tree.contains(-88));
+
+  // get
+  for (int i = 0; i < 8; ++i) {
+    EXPECT_EQ(*tree.get(i), i);
+  }
+  EXPECT_EQ(tree.get(88), nullptr);
+  EXPECT_EQ(tree.get(-88), nullptr);
+
+  EXPECT_EQ(tree.size(), 8);
+}
+
+TEST(RBTree, randomPut)
+{
+  RBTree<int, int> tree;
+  std::vector<int> input;
+  input.reserve(10);
+  for (int i = 0; i < 10; ++i) {
+    input.emplace_back(i);
+  }
+
+  std::shuffle(input.begin(), input.end(), std::mt19937(std::random_device()()));
+
+  for (auto num : input) {
+    tree.put(num, num);
+  }
+
+  EXPECT_EQ(tree.size(), 10);
+  EXPECT_EQ(tree.min(), 0);
+  EXPECT_EQ(tree.max(), 9);
+
+  EXPECT_TRUE(tree.isBST()) << "not a BST\n";
+  EXPECT_TRUE(tree.is23()) << "not a 2-3 tree\n";
+  EXPECT_TRUE(tree.isBalanced()) << "not a balanced tree\n";
 }
 
 int main(int argc, char *argv[])
