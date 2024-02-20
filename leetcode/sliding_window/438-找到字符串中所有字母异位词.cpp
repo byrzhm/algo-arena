b1ca1893ab3d4178b307bd3ee1df13dee971f9a6
zhm_mac <zhm1019@qq.com>
2024-02-20 11:33:03 +0800

    Add solution for finding anagrams using sliding window

---
+++ leetcode/sliding_window/438-找到字符串中所有字母异位词.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+
+class Solution
+{
+public:
+  std::vector<int> findAnagrams(const std::string &s, const std::string &p)
+  {
+    std::vector<int> ans;
+    int sLen = s.length();
+    int pLen = p.length();
+
+    if (sLen < pLen) {
+      return {};
+    }
+
+    std::unique_ptr<int[]> count = std::make_unique<int[]>(26);
+    std::memset(count.get(), 0, 26 * sizeof(int));
+
+    for (int i = 0; i < pLen; ++i) {
+      ++count[s[i] - 'a'];
+      --count[p[i] - 'a'];
+    }
+
+    int diff = 0;
+    for (int i = 0; i < 26; ++i) {
+      if (count[i] != 0)
+        ++diff;
+    }
+
+    if (diff == 0) {
+      ans.emplace_back(0);
+    }
+
+    for (int i = 0; i < sLen - pLen; ++i) {
+      int j = i + pLen;
+      // change diff before: 👇
+      // ++count[s[j] - 'a'];
+      // --count[s[i] - 'a'];
+
+      if (count[s[i] - 'a'] == 1) {
+        --diff;
+      } else if (count[s[i] - 'a'] == 0) {
+        ++diff;
+      }
+      // else diff doesn't change
+
+      --count[s[i] - 'a'];
+
+      if (count[s[j] - 'a'] == -1) {
+        --diff;
+      } else if (count[s[j] - 'a'] == 0) {
+        ++diff;
+      }
+      // else diff doesn't change
+
+      ++count[s[j] - 'a'];
+
+      if (diff == 0) {
+        ans.emplace_back(i + 1);
+      }
+    }
+    return ans;
+  }
+};
+
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  for (auto x : Solution().findAnagrams("cbaebabacd", "abc")) {
+    std::cout << x << '\n';
+  }
+
+  std::cout << '\n';
+
+  for (auto x : Solution().findAnagrams("bpaa", "aa")) {
+    std::cout << x << '\n';
+  }
+
+  return 0;
+}
