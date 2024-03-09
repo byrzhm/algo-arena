b0b9241f6d0e3b78bd559955a48b7dfe59b07729
byrzhm <92535690+byrzhm@users.noreply.github.com>
2024-03-09 23:50:44 +0800

    bugfix: use dp instead

--- algs4-princeton/seam/SeamCarver.java
+++ algs4-princeton/seam/SeamCarver.java
@@ -1,23 +1,23 @@
 import edu.princeton.cs.algs4.Picture;
-import edu.princeton.cs.algs4.Queue;
-import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
 
 import java.awt.Color;
 
 public class SeamCarver {
     private Picture picture;
+    private static final double DEFAULT_ENERGY = 1000.0;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
         if (picture == null) {
             throw new IllegalArgumentException("picture is null");
         }
-        this.picture = picture;
+        this.picture = new Picture(picture);
     }
 
     // current picture
     public Picture picture() {
-        return picture;
+        return new Picture(picture);
     }
 
     // width of current picture
@@ -36,20 +36,20 @@ public class SeamCarver {
             throw new IllegalArgumentException("Either x or y is outside its prescribed range");
         }
         if (x == 0 || x == width() - 1 || y == 0 || y == height() - 1) {
-            return 1000.0;
+            return DEFAULT_ENERGY;
         }
 
-        Color left = picture.get(x - 1, y);
-        Color right = picture.get(x + 1, y);
-        Color top = picture.get(x, y - 1);
-        Color down = picture.get(x, y + 1);
+        int left = picture.get(x - 1, y).getRGB();
+        int right = picture.get(x + 1, y).getRGB();
+        int top = picture.get(x, y - 1).getRGB();
+        int down = picture.get(x, y + 1).getRGB();
 
-        int redX = left.getRed() - right.getRed();
-        int redY = top.getRed() - down.getRed();
-        int greenX = left.getGreen() - right.getGreen();
-        int greenY = top.getGreen() - down.getGreen();
-        int blueX = left.getBlue() - right.getBlue();
-        int blueY = top.getBlue() - down.getBlue();
+        int redX = ((left >> 16) & 0xff) - ((right >> 16) & 0xff);
+        int redY = ((top >> 16) & 0xff) - ((down >> 16) & 0xff);
+        int greenX = ((left >> 8) & 0xff) - ((right >> 8) & 0xff);
+        int greenY = ((top >> 8) & 0xff) - ((down >> 8) & 0xff);
+        int blueX = (left & 0xff) - (right & 0xff);
+        int blueY = (top & 0xff) - (down & 0xff);
 
         double squaredEnergy = 0.0;
         squaredEnergy += redX * redX + greenX * greenX + blueX * blueX;
@@ -60,160 +60,133 @@ public class SeamCarver {
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
-        double minDist = Double.POSITIVE_INFINITY;
         double[][] distTo = new double[width()][height()];
         int[][] edgeTo = new int[width()][height()];
-        int[] seam = new int[width()];
-        Queue<Integer> queue = new Queue<>();
-        Stack<Integer> stack = new Stack<>();
+        int width = width();
+        int height = height();
 
-        for (int src = 0; src < height(); ++src) { // source is (0, src)
-            for (int i = 0; i < width(); ++i) {
-                for (int j = 0; j < height(); ++j) {
-                    distTo[i][j] = Double.POSITIVE_INFINITY;
-                }
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                distTo[x][y] = Double.POSITIVE_INFINITY;
             }
-            distTo[0][src] = 1000.0; // energy
-            queue.enqueue(src);
-            for (int x = 0; x < width() - 1; ++x) {
-                int size = queue.size();
-                for (int i = 0; i < size; ++i) {
-                    int y = queue.dequeue(); // (x, y)
-                    double temp;
+        }
 
-                    if (y - 1 >= 0) { // (x, y) -> (x + 1, y - 1)
-                        temp = distTo[x][y] + energy(x + 1, y - 1);
-                        if (distTo[x + 1][y - 1] > temp) {
-                            distTo[x + 1][y - 1] = temp;
-                            edgeTo[x + 1][y - 1] = y;
-                        }
-                        queue.enqueue(y - 1);
+        for (int y = 0; y < height; ++y) {
+            distTo[0][y] = energy(0, y);
+        }
+
+        for (int x = 1; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                double energy = energy(x, y);
+                double temp;
+
+                // (x-1, y-1) -> (x, y)
+                if (y - 1 >= 0) {
+                    temp = energy + distTo[x - 1][y - 1];
+                    if (temp < distTo[x][y]) {
+                        distTo[x][y] = temp;
+                        edgeTo[x][y] = y - 1;
                     }
-
-                    if (y + 1 < height()) { // (x, y) -> (x + 1, y + 1)
-                        temp = distTo[x][y] + energy(x + 1, y + 1);
-                        if (distTo[x + 1][y + 1] > temp) {
-                            distTo[x + 1][y + 1] = temp;
-                            edgeTo[x + 1][y + 1] = y;
-                        }
-                        queue.enqueue(y + 1);
-                    }
-
-                    // (x, y) -> (x + 1, y)
-                    temp = distTo[x][y] + energy(x + 1, y);
-                    if (distTo[x + 1][y] > temp) {
-                        distTo[x + 1][y] = temp;
-                        edgeTo[x + 1][y] = y;
-                    }
-                    queue.enqueue(y);
-                }
-            }
-
-            int minIdx = -1;
-            while (!queue.isEmpty()) {
-                int dst = queue.dequeue();
-                if (distTo[width() - 1][dst] < minDist) {
-                    minIdx = dst;
-                    minDist = distTo[width() - 1][dst];
-                }
-            }
-
-            if (minIdx > 0) {
-                int y = minIdx;
-                for (int x = width() - 1; x >= 0; --x) {
-                    stack.push(y);
-                    y = edgeTo[x][y];
                 }
 
-                int i = 0;
-                while (!stack.isEmpty()) {
-                    y = stack.pop();
-                    seam[i] = y;
-                    ++i;
+                // (x-1, y) -> (x, y)
+                temp = energy + distTo[x - 1][y];
+                if (temp < distTo[x][y]) {
+                    distTo[x][y] = temp;
+                    edgeTo[x][y] = y;
+                }
+
+                // (x-1, y+1) -> (x, y)
+                if (y + 1 < height) {
+                    temp = energy + distTo[x - 1][y + 1];
+                    if (temp < distTo[x][y]) {
+                        distTo[x][y] = temp;
+                        edgeTo[x][y] = y + 1;
+                    }
                 }
             }
         }
 
+        int[] seam = new int[width()];
+        int minY = -1;
+        double minDist = Double.POSITIVE_INFINITY;
+        for (int y = 0; y < height; ++y) {
+            if (distTo[width - 1][y] < minDist) {
+                minDist = distTo[width - 1][y];
+                minY = y;
+            }
+        }
+        int y = minY;
+        for (int x = width - 1; x >= 0; --x) {
+            seam[x] = y;
+            y = edgeTo[x][y];
+        }
         return seam;
     }
 
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
-        double minDist = Double.POSITIVE_INFINITY;
-        int[] seam = new int[height()];
         double[][] distTo = new double[width()][height()];
         int[][] edgeTo = new int[width()][height()];
-        Queue<Integer> queue = new Queue<>();
-        Stack<Integer> stack = new Stack<>();
+        int width = width();
+        int height = height();
 
-        for (int src = 0; src < width(); ++src) { // source is (src, 0)
-            for (int i = 0; i < width(); ++i) {
-                for (int j = 0; j < height(); ++j) {
-                    distTo[i][j] = Double.POSITIVE_INFINITY;
-                }
-            }
-            distTo[src][0] = 1000.0; // energy
-            queue.enqueue(src);
-            for (int y = 0; y < height() - 1; ++y) {
-                int size = queue.size();
-                for (int i = 0; i < size; ++i) {
-                    int x = queue.dequeue(); // (x, y)
-                    double temp;
-
-                    if (x - 1 >= 0) { // (x, y) -> (x - 1, y + 1)
-                        temp = distTo[x][y] + energy(x - 1, y + 1);
-                        if (distTo[x - 1][y + 1] > temp) {
-                            distTo[x - 1][y + 1] = temp;
-                            edgeTo[x - 1][y + 1] = x;
-                        }
-                        queue.enqueue(x - 1);
-                    }
-
-                    if (x + 1 < width()) { // (x, y) -> (x + 1, y + 1)
-                        temp = distTo[x][y] + energy(x + 1, y + 1);
-                        if (distTo[x + 1][y + 1] > temp) {
-                            distTo[x + 1][y + 1] = temp;
-                            edgeTo[x + 1][y + 1] = x;
-                        }
-                        queue.enqueue(x + 1);
-                    }
-
-                    // (x, y) -> (x, y + 1)
-                    temp = distTo[x][y] + energy(x, y + 1);
-                    if (distTo[x][y + 1] > temp) {
-                        distTo[x][y + 1] = temp;
-                        edgeTo[x][y + 1] = x;
-                    }
-                    queue.enqueue(x);
-                }
-            }
-
-            int minIdx = -1;
-            while (!queue.isEmpty()) { // (dst, height - 1)
-                int dst = queue.dequeue();
-                if (distTo[dst][height() - 1] < minDist) {
-                    minIdx = dst;
-                    minDist = distTo[dst][height() - 1];
-                }
-            }
-
-            if (minIdx > 0) {
-                int x = minIdx;
-                for (int y = height() - 1; y >= 0; --y) {
-                    stack.push(x);
-                    x = edgeTo[x][y];
-                }
-
-                int i = 0;
-                while (!stack.isEmpty()) {
-                    x = stack.pop();
-                    seam[i] = x;
-                    ++i;
-                }
-
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                distTo[x][y] = Double.POSITIVE_INFINITY;
             }
         }
 
+        for (int x = 0; x < width; ++x) {
+            distTo[x][0] = energy(x, 0);
+        }
+
+        for (int y = 1; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                double energy = energy(x, y);
+                double temp;
+
+                // (x-1, y-1) -> (x, y)
+                if (x - 1 >= 0) {
+                    temp = energy + distTo[x - 1][y - 1];
+                    if (temp < distTo[x][y]) {
+                        distTo[x][y] = temp;
+                        edgeTo[x][y] = x - 1;
+                    }
+                }
+
+                temp = energy + distTo[x][y - 1];
+                if (temp < distTo[x][y]) {
+                    distTo[x][y] = temp;
+                    edgeTo[x][y] = x;
+                }
+
+                // (x+1, y-1) -> (x, y)
+                if (x + 1 < width) {
+                    temp = energy + distTo[x + 1][y - 1];
+                    if (temp < distTo[x][y]) {
+                        distTo[x][y] = temp;
+                        edgeTo[x][y] = x + 1;
+                    }
+                }
+            }
+        }
+
+
+        double minDist = Double.POSITIVE_INFINITY;
+        int[] seam = new int[height()];
+        int minX = -1;
+        for (int x = 0; x < width; ++x) {
+            if (distTo[x][height - 1] < minDist) {
+                minDist = distTo[x][height - 1];
+                minX = x;
+            }
+        }
+        int x = minX;
+        for (int y = height - 1; y >= 0; --y) {
+            seam[y] = x;
+            x = edgeTo[x][y];
+        }
         return seam;
     }
 
@@ -226,11 +199,16 @@ public class SeamCarver {
             throw new IllegalArgumentException("height <= 1");
         }
         int size = 0;
+        int prev = -1;
         for (int y : seam) {
             ++size;
             if (y < 0 || y >= height()) {
                 throw new IllegalArgumentException("not a valid seam");
             }
+            if (prev >= 0 && Math.abs(y - prev) > 1) {
+                throw new IllegalArgumentException("not a valid seam");
+            }
+            prev = y;
         }
         if (size != width()) {
             throw new IllegalArgumentException("not a valid seam");
@@ -258,11 +236,16 @@ public class SeamCarver {
             throw new IllegalArgumentException("width <= 1");
         }
         int size = 0;
+        int prev = -1;
         for (int x : seam) {
             ++size;
             if (x < 0 || x >= width()) {
                 throw new IllegalArgumentException("not a valid seam");
             }
+            if (prev >= 0 && Math.abs(x - prev) > 1) {
+                throw new IllegalArgumentException("not a valid seam");
+            }
+            prev = x;
         }
         if (size != height()) {
             throw new IllegalArgumentException("not a valid seam");
@@ -279,5 +262,12 @@ public class SeamCarver {
             }
         }
         picture = newPicture;
+    }
+
+    public static void main(String[] args) {
+        StdOut.printf("%x\n", Color.BLACK.getRGB());
+        StdOut.printf("%x\n", Color.RED.getRGB());
+        StdOut.printf("%x\n", Color.GREEN.getRGB());
+        StdOut.printf("%x\n", Color.BLUE.getRGB());
     }
 }
