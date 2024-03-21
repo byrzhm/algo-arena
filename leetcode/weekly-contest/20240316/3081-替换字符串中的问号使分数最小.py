f76d48ecbe3bc6ecef1f1a5e94646691541f6b56
zhm_mac <zhm1019@qq.com>
2024-03-21 10:46:24 +0800

    solve this!

---
+++ leetcode/weekly-contest/20240316/3081-替换字符串中的问号使分数最小.py
@@ -0,0 +1,5 @@
+
+# TODO: solve this
+class Solution:
+    def minimizeStringValue(self, s: str) -> str:
+        pass
---
+++ leetcode/weekly-contest/20240316/3082-求出所有子序列的能量和.py
@@ -0,0 +1,6 @@
+from typing import List
+
+# TODO: solve this
+class Solution:
+    def sumOfPower(self, nums: List[int], k: int) -> int:
+        pass
