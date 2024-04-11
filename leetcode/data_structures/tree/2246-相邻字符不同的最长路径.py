54199150ef7e7c9fa856db28f8c0e02bc7a45e23
zhm_mac <zhm1019@qq.com>
2024-04-11 14:00:19 +0800

    Add solution for longestPath problem in tree/2246.py

---
+++ leetcode/data_structures/tree/2246-相邻字符不同的最长路径.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def longestPath(self, parent: List[int], s: str) -> int:
+        n = len(parent)
+        adj = [[] for _ in range(n)]
+
+        for i in range(1, n):
+            p = parent[i]
+            adj[p].append(i)
+        
+        ans = 1
+
+        def dfs(i):
+            nonlocal ans
+
+            first, second = 0, 0
+            for j in adj[i]:
+                second = dfs(j)
+                if s[j] != s[i]:
+                    ans = max(ans, first + second + 1)
+                    first = max(first, second)
+            return first + 1
+
+        dfs(0)
+        return ans
