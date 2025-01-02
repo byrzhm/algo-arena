38a7dfcb89089f7423643bb49669f01281f3f133
zhm_mac <zhm1019@qq.com>
2025-01-02 23:34:49 +0800

    leetcode 729

---
+++ problems/greedy/lc_729.py
@@ -0,0 +1,34 @@
+import bisect
+from typing import List, Tuple
+
+
+class MyCalendar:
+
+    def __init__(self):
+        self.arr: List[Tuple[int, int]] = []
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        n = len(self.arr)
+        idx = bisect.bisect_left(self.arr, (endTime, startTime))
+        previous, next = False, False
+
+        if idx == 0 or self.arr[idx - 1][0] <= startTime:
+            previous = True
+        if idx == n or self.arr[idx][1] >= endTime:
+            next = True
+        if previous and next:
+            self.arr.insert(idx, (endTime, startTime))
+            return True
+        return False
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
+
+obj = MyCalendar()
+assert True == obj.book(37, 50)
+assert False == obj.book(33, 50)
+assert True == obj.book(4, 17)
+assert False == obj.book(35, 48)
+assert False == obj.book(8, 25)
