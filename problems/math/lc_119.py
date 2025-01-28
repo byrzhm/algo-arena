a7af15ba9258ee8e7b2e4b8414d881f403a4b5f3
zhm_mac <zhm1019@qq.com>
2025-01-28 13:15:03 +0800

    leetcode 119 another solution

--- problems/math/lc_119.py
+++ problems/math/lc_119.py
@@ -15,3 +15,11 @@ for i in range(2, 34):
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         return f[rowIndex]
+
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1] * (rowIndex + 1)
+        for i in range(1, rowIndex + 1):
+            row[i] = row[i - 1] * (rowIndex - i + 1) // i
+        return row
