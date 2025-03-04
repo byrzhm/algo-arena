6705be581d75ea4fddecd03d6c7b168fe92322e2
Hongming Zhu <zhm1019@qq.com>
2025-03-04 11:13:33 +0800

    leetcode 189

---
+++ problems/data_structures/array/lc_189.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums[:] = nums[-k:] + nums[:-k]  # nums = ... 是不会修改原数组的
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        def reverse(l: int, r: int) -> None:
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l, r = l + 1, r - 1
+
+        n = len(nums)
+        k = k % n
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
