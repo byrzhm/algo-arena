029cb733d639e84f80718dc6d135c08a6c8a550b
Hongming Zhu <zhm1019@qq.com>
2025-04-16 09:18:27 +0800

    leetcode 2537

---
+++ problems/sliding_window/lc_2537.py
@@ -0,0 +1,17 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def countGood(self, nums: List[int], k: int) -> int:
+        cnt = defaultdict(int)  # 比 Counter() 快
+        ans = left = pairs = 0
+        for x in nums:
+            pairs += cnt[x]
+            cnt[x] += 1
+            while pairs >= k:
+                cnt[nums[left]] -= 1
+                pairs -= cnt[nums[left]]
+                left += 1
+            ans += left
+        return ans
